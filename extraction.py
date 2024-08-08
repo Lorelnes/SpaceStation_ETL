581eec8fbc53ec40a1834a0c995134f0c38709ca
Lorelnes <nataananidze@gmail.com>
2024-08-08 15:58:19 +0400

    Add error handling to extraction code

--- extraction.py
+++ extraction.py
@@ -1,10 +1,26 @@
+from urllib.error import HTTPError, URLError
 from constants import url
 from urllib import request
 import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def extract_data(url):
-    response = request.urlopen(url)
-    data = json.loads(response.read().decode())
+    try:
+        response = request.urlopen(url)
+        data = json.dumps(response.read().decode())
+        response.close()
+        return json.loads(data)
+    except HTTPError as e:
+        logging.error(f'HTTPError occured: {e}')
+    except URLError as e:
+        logging.error(f'URLError occured: {e}')
+    except TimeoutError as e:
+        logging.error(f'TimeoutError occured: {e}')
+    except Exception as e:
+        logging.error(f'An unexpected error occured: {e}')
+
     return data
 
 
